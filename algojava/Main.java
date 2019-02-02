5547c81ec2dbb616d5d735afefd537c88fe2f3be
danila <daniil.perlov@algoritmika.org>
2019-02-02 22:09:28 +0300

    Sedgwick bst tree project initialization

---
+++ algojava/Main.java
@@ -0,0 +1,24 @@
+package algojava;
+
+public class Main {
+
+    /**
+     * Unit tests the {@code RedBlackBST} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) { 
+        RedBlackBST<String, Integer> st = new RedBlackBST<String, Integer>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+        for (String s : st.keys())
+            StdOut.println(s + " " + st.get(s));
+        StdOut.println();
+
+        // Queue<String> keys = new Queue<String>();
+        Iterable<String> keys = st.keys("A", "X");
+        StdOut.println(keys);
+    }
+}
